4108e827cf5d05f5eaa4338e7e4e7584b2254d0f
maarcusvinicius <marcus.editor77@gmail.com>
2022-09-30 14:07:08 -0300

    database serveer

--- server/src/server.ts
+++ server/src/server.ts
@@ -32,7 +32,7 @@ app.get('/games', async (request, response) => {
 
 app.post('/games/:id/ads', async (request, response) => {
     const gameId = request.params.id
-    const body : any = request.body
+    const body: any = request.body
 
 
     const ad = await prisma.ad.create({
